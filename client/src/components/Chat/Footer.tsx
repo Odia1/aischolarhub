7a89991bf1ea7e063d13d832761f056084e42f2f
Prof. Patra <ppatra@seedsnet.org>
2026-08-24 18:24:27 -0700

    Working robust customized AI Scholar Hub and backup enabled

--- client/src/components/Chat/Footer.tsx
+++ client/src/components/Chat/Footer.tsx
@@ -39,9 +39,7 @@ function Footer({ className, startupConfig }: FooterProps) {
   const mainContentParts = (
     typeof config?.customFooter === 'string'
       ? config.customFooter
-      : '[LibreChat ' +
-        Constants.VERSION +
-        '](https://librechat.ai) - ' +
+      : 
         localize('com_ui_latest_footer')
   ).split('|');
 
